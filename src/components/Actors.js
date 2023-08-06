4850449446f0b5cf63065ec793f205f2042a595e
steve-the-hermit <stevemacharia309@gmail.com>
2023-08-07 00:09:43 +0300

    completed

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,25 @@
 import React from "react";
-import { actors } from "../data";
+import { actors, movies } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map(actor => (
+        <div key={actor.id}>
+          <h2>{actor.name}</h2>
+          <p>Movies:</p>
+          <ul>
+            {movies
+              .filter(movie => movie.actorIds.includes(actor.id))
+              .map((movie, index) => (
+                <li key={index}>{movie.title}</li>
+              ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,25 @@
 import React from "react";
-import { directors } from "../data";
+import { directors, movies } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map(director => (
+        <div key={director.id}>
+          <h2>{director.name}</h2>
+          <p>Movies:</p>
+          <ul>
+            {movies
+              .filter(movie => movie.directorId === director.id)
+              .map((movie, index) => (
+                <li key={index}>{movie.title}</li>
+              ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Directors;
